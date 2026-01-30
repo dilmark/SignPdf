01aeb5b2b342e7d4c6e4c10bb5e1aa8ad65f0b61
Mariusz Dyla <dilmark@wp.pl>
2026-01-30 12:29:09 +0100

    Modernization of cli mode - launching without GUI

--- SignPdf.py
+++ SignPdf.py
@@ -17,11 +17,14 @@ https://www.gnu.org/licenses/gpl-3.0.en.html
 """
 
 import logging
+import argparse
 import os
 import sys
 
 from pathlib import Path
 from ui.app_window import SignPdfApp
+from core.pdf_sign import PdfSigner
+from config.app_config import app_config
 
 # Wyłącz IBus dla tej aplikacji - nie będzie wisiało przy zamykaniu
 os.environ["GTK_IM_MODULE"] = "none"
@@ -39,18 +42,76 @@ def main():
     logging.info("START aplikacji")
 
     initial_pdf = None
+    args = parse_args()
 
-    if len(sys.argv) > 1:
-        candidate = Path(sys.argv[1]).expanduser()
-        if candidate.exists() and candidate.is_file() and candidate.suffix.lower() == ".pdf":
-            initial_pdf = candidate
-            logging.info(f"CLI: podano plik PDF: {initial_pdf}")
-        else:
-            logging.warning(f"CLI: nieprawidłowy argument: {sys.argv[1]}")
+    if args.cli:
+        if not args.pdf:
+            print("❌ Brak pliku PDF")
+            return
 
+        exit_code = sign_no_gui(Path(args.pdf).expanduser())
+        return exit_code
+    if (
+        args.pdf
+        and Path(args.pdf).exists()
+        and Path(args.pdf).is_file()
+        and Path(args.pdf).suffix.lower() == ".pdf"
+    ):
+        initial_pdf = Path(args.pdf).expanduser()
+    else:
+        initial_pdf = None
     app = SignPdfApp(initial_pdf=initial_pdf)
     app.run()
+
     logging.info("STOP aplikacji")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Podpis elektroniczny dokumentów PDF")
+    parser.add_argument(
+        "pdf",
+        nargs="?",
+        help="Ścieżka do pliku PDF",
+    )
+    parser.add_argument(
+        "--no_gui", "--cli", "-c", "-ng",
+        dest="cli",
+        action="store_true",
+        help="Podpisz dokument bez uruchamiania GUI",
+    )
+    return parser.parse_args()
+
+
+def sign_no_gui(pdf_path: Path):
+    if not pdf_path.exists():
+        print(f"❌ Plik nie istnieje: {pdf_path}")
+        return 1
+
+    output_pdf = pdf_path.with_name(pdf_path.stem + "_sign.pdf")
+
+    signer = PdfSigner(
+        parent=None,  # brak GUI
+        pdf_stamp=Path("/tmp"),  # brak stempla
+        pdf_orig=str(pdf_path),
+        pdf_final=str(output_pdf),
+        logo_path=app_config.data["config_logo"],
+        comment=app_config.data["config_comment"],
+        cert=app_config.data["config_cert"],
+        page_index=0,
+        width=0,
+        height=0,
+        use_visual_stamp=False,
+        on_done=None,
+    )
+
+    try:
+        signer.sign()
+    except Exception as e:
+        print(f"❌ Błąd podpisu: {e}")
+        return 2
+
+    print(f"✅ Podpisano dokument: {output_pdf}")
+    return 0
 
 
 if __name__ == "__main__":
